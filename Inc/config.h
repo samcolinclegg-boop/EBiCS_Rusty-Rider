1c3d6e2321415d7089a944ccf0170f3c9297b42e
samcolinclegg-boop <samcolinclegg@gmail.com>
2026-04-07 12:18:19 +1000

    RustyRider12.h

--- Inc/config.h
+++ Inc/config.h
@@ -103,7 +103,7 @@
 
 //---------------------------------------------------------------------
 //Display settings
-#define DISPLAY_TYPE (DISPLAY_TYPE_KUNTENG)
+#define DISPLAY_TYPE DISPLAY_TYPE_NO2
 
 //---------------------------------------------------------------------
 //Regen settings
