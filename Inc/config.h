811420217a60f040e695b18b18c473bb5c474b22
samcolinclegg-boop <samcolinclegg@gmail.com>
2026-08-03 22:50:51 +1000

    Update GEAR_RATIO from 120 to 123

--- Inc/config.h
+++ Inc/config.h
@@ -83,7 +83,7 @@
 //--------------------------------------------------------------------
 //Speed settings
 #define WHEEL_CIRCUMFERENCE 2200
-#define GEAR_RATIO 120 //11 for BionX IGH3
+#define GEAR_RATIO 123 //11 for BionX IGH3
 #define SPEEDLIMIT 30
 #define PULSES_PER_REVOLUTION 1
 #define SPEEDSOURCE INTERNAL
