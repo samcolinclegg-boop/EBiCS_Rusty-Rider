9a116be030ede7b4ed3965fbc82e81f7f7788255
samcolinclegg-boop <samcolinclegg@gmail.com>
2026-08-16 14:46:18 +1000

    Change REVERSE definition to -1 for reverse direction

--- Inc/config.h
+++ Inc/config.h
@@ -91,7 +91,7 @@
 //power settings
 #define PH_CURRENT_MAX 1200
 #define BATTERYCURRENT_MAX 15000
-#define REVERSE 1 //1 for normal direction, -1 for reverse
+#define REVERSE -1 //1 for normal direction, -1 for reverse
 #define PUSHASSIST_CURRENT 300
 #define VOLTAGE_MIN 1560 //39V
 
