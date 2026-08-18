0a8b95750c2c6e512825cf455aff74ba00129eea
samcolinclegg-boop <samcolinclegg@gmail.com>
2026-08-18 14:03:43 +1000

    Autodetect 6/101 No reverse

--- Inc/config.h
+++ Inc/config.h
@@ -81,17 +81,17 @@
 //Speed settings
 #define WHEEL_CIRCUMFERENCE 2200
 #define GEAR_RATIO 101 //11 for BionX IGH3
-#define SPEEDLIMIT 30
-#define PULSES_PER_REVOLUTION 3
+#define SPEEDLIMIT 25
+#define PULSES_PER_REVOLUTION 6
 #define SPEEDSOURCE INTERNAL
 #define SPEEDFILTER 1
 #define SPDSHFT 0
 
 //---------------------------------------------------------------------
 //power settings
-#define PH_CURRENT_MAX 1200
+#define PH_CURRENT_MAX 1500
 #define BATTERYCURRENT_MAX 15000
-#define REVERSE -1 //1 for normal direction, -1 for reverse
+#define REVERSE 1 //1 for normal direction, -1 for reverse
 #define PUSHASSIST_CURRENT 300
 #define VOLTAGE_MIN 1560 //39V
 
@@ -103,7 +103,7 @@
 
 //---------------------------------------------------------------------
 //Display settings
-#define DISPLAY_TYPE DISPLAY_TYPE_NO2
+#define DISPLAY_TYPE DISPLAY_TYPE_DEBUG
 
 //---------------------------------------------------------------------
 //Regen settings
@@ -113,6 +113,6 @@
 //#define ADC_BRAKE
 
 //---------------------------------------------------------------------
-#define AUTODETECT 0
+#define AUTODETECT 1
 
 #endif /* CONFIG_H_ */
